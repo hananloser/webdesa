6c462540e01d029067de0432325fdbccc46779b9
= <=>
2020-03-15 03:09:20 +0800

    Add New Asset2 for front end

--- resources/views/components/landing/Navbar.blade.php
+++ resources/views/components/landing/Navbar.blade.php
@@ -3,11 +3,10 @@
         <div class="navbar-translate">
             <a class="navbar-brand" href="https://demos.creative-tim.com/paper-kit/index.html" rel="tooltip"
                 title="Coded by Creative Tim" data-placement="bottom" target="_blank">
-                Paper Kit 2
+                Bangun Jaya
             </a>
-            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar bar1"></span>
                 <span class="navbar-toggler-bar bar2"></span>
                 <span class="navbar-toggler-bar bar3"></span>
@@ -16,39 +15,11 @@
         <div class="collapse navbar-collapse justify-content-end" id="navigation">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a href="../index.html" class="nav-link"><i class="nc-icon nc-layout-11"></i> Components</a>
+                    <a href="../index.html" class="nav-link"><i class="nc-icon nc-layout-11"></i> Layanan </a>
                 </li>
                 <li class="nav-item">
                     <a href="https://demos.creative-tim.com/paper-kit-2/docs/1.0/getting-started/introduction.html"
-                        target="_blank" class="nav-link"><i class="nc-icon nc-book-bookmark"></i> Documentation</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom"
-                        href="https://twitter.com/CreativeTim" target="_blank">
-                        <i class="fa fa-twitter"></i>
-                        <p class="d-lg-none">Twitter</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom"
-                        href="https://www.facebook.com/CreativeTim" target="_blank">
-                        <i class="fa fa-facebook-square"></i>
-                        <p class="d-lg-none">Facebook</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom"
-                        href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
-                        <i class="fa fa-instagram"></i>
-                        <p class="d-lg-none">Instagram</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Star on GitHub" data-placement="bottom"
-                        href="https://www.github.com/CreativeTimOfficial" target="_blank">
-                        <i class="fa fa-github"></i>
-                        <p class="d-lg-none">GitHub</p>
-                    </a>
+                        target="_blank" class="nav-link"><i class="nc-icon nc-book-bookmark"></i>Pengaduan</a>
                 </li>
             </ul>
         </div>
