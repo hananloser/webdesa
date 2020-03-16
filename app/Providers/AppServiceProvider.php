7a077e6bef0b345c1914e8544ac6355a0d3804fe
= <=>
2020-03-17 03:59:40 +0800

    tes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            $url->forceSchema('http');
         }
 
         Gate::define('isAdmin', function ($user) {
