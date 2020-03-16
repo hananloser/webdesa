ceaac4320ddbcdd865c10e9fdcaa78699442975a
= <=>
2020-03-17 03:41:45 +0800

    new Setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
+
         Gate::define('isAdmin', function ($user) {
             return $user->role === 'admin';
         });
