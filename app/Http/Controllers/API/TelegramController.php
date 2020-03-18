38af8398584d9612b1dcbecad620601ee316a2a8
= <=>
2020-03-19 01:16:53 +0800

    Get Updates

--- app/Http/Controllers/API/TelegramController.php
+++ app/Http/Controllers/API/TelegramController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
 
     public function getId(){
-        return 'oke';
+
+        $res = Telegram::getUpdates();
+
+        return $res ;
+
     }
 
 }
