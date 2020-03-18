49bbe96e8c702b3cd70cd5fc9c63095a77fea095
= <=>
2020-03-19 01:16:03 +0800

    Test To Server

--- app/Http/Controllers/API/TelegramController.php
+++ app/Http/Controllers/API/TelegramController.php
@@ -3,41 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Telegram\Bot\Laravel\Facades\Telegram;
+use Illuminate\Http\Request;
 
 class TelegramController extends Controller
 {
 
-    public function getId()
-    {
-        $tele = Telegram::getUpdates();
-        $chat_id = array();
-        foreach ($tele as $item) {
-            $newitem = $item['message']['chat']['id'];
-            array_push($chat_id, $newitem);
-        }
-        return array_unique($chat_id);
+    public function getId(){
+        return 'oke';
     }
-
-    public function kirim()
-    {
-        foreach ($this->testApi() as $value) {
-            $response = Telegram::sendMessage([
-                'chat_id' => $value,
-                'text' => 'Test Satu kali Lagi',
-            ]);
-        }
-        return response()->json([
-            'status' => 'Pesan Telah Di Kirim',
-        ]);
-    }
-
-
-    public function webhook(){
-        $updates = Telegram::getWebhookUpdates();
-        Telegram::commandsHandler(true);
-    }
-
-
 
 }
