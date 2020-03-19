ebafac98b9a855b6c4aac359f53d434133569445
= <=>
2020-03-19 16:14:13 +0800

    add particle js

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,66 +1,68 @@
 @extends('layouts.landing')
 @section('content')
 <div class="wrapper bg-dark">
-    <div class="container-fluid">
+    <div id="particles-js">
+        <div class="container-fluid">
+            <br>
+            <div class="row justify-content-center ">
+                <center>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <img src="{{asset('assets/img/logo.png')}}" height="150">
+                    </div>
+                </center>
+            </div>
+            <div class="row justify-content-center ">
+                <blockquote class="blockquote text-center">
+                    <h2 class="h3 text-white text-bold">PEMERINTAH KABUPATEN LUWU TIMUR</h2>
+                    <h3 class="text-white">DESA BANGUN JAYA</h3>
+                </blockquote>
+            </div>
+        </div>
+        <div class="divider"></div>
         <br>
-        <div class="row justify-content-center ">
-            <center>
-                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <img src="{{asset('assets/img/logo.png')}}" height="150">
+        <br>
+        <div class="container mr-3">
+            <div class="row flex-row">
+                <div class="col">
+                    <a href="">
+                        <i class="fa fa-4x fa-user ml-4 text-white text-center"></i>
+                        <h5 class="text-white">Profile Desa</h5>
+                    </a>
                 </div>
-            </center>
-        </div>
-        <div class="row justify-content-center ">
-            <blockquote class="blockquote text-center">
-                <h2 class="h3 text-white text-bold">PEMERINTAH KABUPATEN LUWU TIMUR</h2>
-                <h3 class="text-white">DESA BANGUN JAYA</h3>
-            </blockquote>
-        </div>
-    </div>
-    <div class="divider"></div>
-    <br>
-    <br>
-    <div class="container mr-3">
-        <div class="row flex-row">
-            <div class="col">
-                <a href="">
-                    <i class="fa fa-4x fa-user ml-4 text-white text-center"></i>
-                    <h5 class="text-white">Profile Desa</h5>
-                </a>
-            </div>
-            <div class="col">
-                <a href="">
-                    <i class="fa fa-4x fa-eye ml-3 text-danger text-center"></i>
-                    <h5 class="text-white">Visi & Misi</h5>
-                </a>
-            </div>
-            <div class="col">
-                <i class="fa fa-4x fa-bar-chart text-warning"></i>
-                <h5 class="text-white">Profile</h5>
-            </div>
-            <div class="col">
-                <i class="fa fa-4x fa-bar-chart text-danger"></i>
-                <h5 class="text-white">Profile</h5>
+                <div class="col">
+                    <a href="">
+                        <i class="fa fa-4x fa-eye ml-3 text-danger text-center"></i>
+                        <h5 class="text-white">Visi & Misi</h5>
+                    </a>
+                </div>
+                <div class="col">
+                    <i class="fa fa-4x fa-bar-chart text-warning"></i>
+                    <h5 class="text-white">Profile</h5>
+                </div>
+                <div class="col">
+                    <i class="fa fa-4x fa-bar-chart text-danger"></i>
+                    <h5 class="text-white">Profile</h5>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container mr-3">
-        <div class="row flex-row">
-            <div class="col">
-                <i class="fa fa-4x fa-bar-chart text-white text-center"></i>
-                <h5 class="text-white">Profile</h5>
-            </div>
-            <div class="col">
-                <i class="fa fa-4x fa-bar-chart text-primary"></i>
-                <h5 class="text-white">Profile</h5>
-            </div>
-            <div class="col">
-                <i class="fa fa-4x fa-bar-chart text-warning"></i>
-                <h5 class="text-white">Profile</h5>
-            </div>
-            <div class="col">
-                <i class="fa fa-4x fa-bar-chart text-danger"></i>
-                <h5 class="text-white">Profile</h5>
+        <div class="container mr-3">
+            <div class="row flex-row">
+                <div class="col">
+                    <i class="fa fa-4x fa-bar-chart text-white text-center"></i>
+                    <h5 class="text-white">Profile</h5>
+                </div>
+                <div class="col">
+                    <i class="fa fa-4x fa-bar-chart text-primary"></i>
+                    <h5 class="text-white">Profile</h5>
+                </div>
+                <div class="col">
+                    <i class="fa fa-4x fa-bar-chart text-warning"></i>
+                    <h5 class="text-white">Profile</h5>
+                </div>
+                <div class="col">
+                    <i class="fa fa-4x fa-bar-chart text-danger"></i>
+                    <h5 class="text-white">Profile</h5>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-<link rel="apple-touch-icon" sizes="76x76" href="">
+    <link rel="apple-touch-icon" sizes="76x76" href="">
     <link rel="icon" type="image/png" href="{{asset('assets/img/logo.png')}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
@@ -20,17 +20,51 @@
     <!-- CSS Just for demo purpose, don't include it in your project -->
 </head>
 
+
 <body class="bg-dark">
     <!-- Navbar -->
     {{-- @include('components.landing.Navbar') --}}
     <!-- End Navbar -->
     <div class="main">
-       @yield('content')
+        @yield('content')
     </div>
     <script src="{{asset('assets2/js/core/jquery.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('assets2/js/core/popper.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('assets2/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('assets2/js/paper-kit.js?v=2.2.0')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/js/particle.js')}}" type="text/javascript"></script>
+
+    <script>
+        particlesJS.load('particles-js', 'assets/js/particlesjs-config.json')
+    </script>
+
+    <style>
+        body,
+        html {
+            height: 100%
+        }
+
+        #particles-js canvas {
+            display: block;
+            vertical-align: bottom;
+            -webkit-transform: scale(1);
+            -ms-transform: scale(1);
+            transform: scale(1);
+            opacity: 1;
+            -webkit-transition: opacity .8s ease, -webkit-transform 1.4s ease;
+            transition: opacity .8s ease, transform 1.4s ease
+        }
+
+        #particles-js {
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            z-index: -10;
+            top: 0;
+            left: 0
+        }
+    </style>
+
 </body>
 
 </html>
